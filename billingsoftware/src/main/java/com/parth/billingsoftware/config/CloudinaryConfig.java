fe084d6489e27faf0aa0f61435503d9a676c9c01
Parth25Kharwade <parthkharwade25@gmail.com>
2025-08-25 21:57:52 +0530

    cludinary service fix

--- billingsoftware/src/main/java/com/parth/billingsoftware/config/CloudinaryConfig.java
+++ billingsoftware/src/main/java/com/parth/billingsoftware/config/CloudinaryConfig.java
@@ -1,6 +1,7 @@
 package com.parth.billingsoftware.config;
 
 import com.cloudinary.Cloudinary;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,13 +10,23 @@ import java.util.Map;
 
 @Configuration
 public class CloudinaryConfig {
+    @Value("${cloudinary.cloud_name}")
+    private String cloudName;
+
+    @Value("${cloudinary.api_key}")
+    private String apiKey;
+
+    @Value("${cloudinary.api_secret}")
+    private String apiSecret;
+
     @Bean
-    public Cloudinary getCloudinary(){
-        Map map =new HashMap();
-        map.put("cloud_name","deuq2mi0r");
-        map.put("api_key","193541318544354");
-        map.put("api_secret","e9bCxbu5xgO5ovUTXdniK52P4QA");
-        map.put("secure",true);
-     return new Cloudinary(map);
-   }
+    public Cloudinary getCloudinary() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("cloud_name", cloudName);
+        map.put("api_key", apiKey);
+        map.put("api_secret", apiSecret);
+        map.put("secure", true);
+
+        return new Cloudinary(map);
+    }
 }
